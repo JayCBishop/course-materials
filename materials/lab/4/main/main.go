cd74e695aa096378a6ce7411200a3888ec788450
Jay Bishop <jay.bishop@dish.com>
2022-03-08 10:37:42 -0700

    Initial lab4 work

--- materials/lab/4/main/main.go
+++ materials/lab/4/main/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 	"wyoassign/wyoassign"
-)
 
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	wyoassign.InitAssignments()
@@ -18,13 +18,12 @@ func main() {
 	router.HandleFunc("/api-status", wyoassign.APISTATUS).Methods("GET")
 	router.HandleFunc("/assignments", wyoassign.GetAssignments).Methods("GET")
 	router.HandleFunc("/assignment/{id}", wyoassign.GetAssignment).Methods("GET")
-	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")		
-	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")	
+	router.HandleFunc("/assignment/{id}", wyoassign.DeleteAssignment).Methods("DELETE")
+	router.HandleFunc("/assignment", wyoassign.CreateAssignment).Methods("POST")
 	// router.HandleFunc("/assignments/{id}", wyoassign.UpdateAssignment).Methods("PUT")
 
 	http.Handle("/", router)
 
 	//start and listen to requests
 	http.ListenAndServe(":8080", router)
-
 }
