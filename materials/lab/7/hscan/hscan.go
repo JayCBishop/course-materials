bed7ba9558c65a0782bedb490eef9e2f59eb41ff
Jay Bishop <jay.bishop@dish.com>
2022-04-12 21:19:51 -0600

    Finishing Lab7

--- materials/lab/7/hscan/hscan.go
+++ materials/lab/7/hscan/hscan.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 //==========================================================================\\
@@ -15,8 +16,7 @@ import (
 var shalookup map[string]string
 var md5lookup map[string]string
 
-func GuessSingle(sourceHash string, filename string) {
-
+func GuessSingle(sourceHash string, filename string) string {
 	f, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln(err)
@@ -28,18 +28,58 @@ func GuessSingle(sourceHash string, filename string) {
 	for scanner.Scan() {
 		password := scanner.Text()
 
-		// TODO - From the length of the hash you should know which one of these to check ...
-		// add a check and logicial structure
-
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (MD5): %s\n", password)
+		var hash string
+		if len(sourceHash) == 32 {
+			hash = fmt.Sprintf("%x", md5.Sum([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (MD5): %s\n", password)
+				return password
+			}
+		} else if len(sourceHash) == 64 {
+			hash = fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+			if hash == sourceHash {
+				fmt.Printf("[+] Password found (SHA-256): %s\n", password)
+				return password
+			}
 		}
+	}
 
-		hash = fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
-		if hash == sourceHash {
-			fmt.Printf("[+] Password found (SHA-256): %s\n", password)
-		}
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	return ""
+}
+
+func GenHashMaps(filename string) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		GenSHAHashMaps(filename)
+		wg.Done()
+	}()
+	go func() {
+		GenMD5HashMaps(filename)
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func GenSHAHashMaps(filename string) {
+	shalookup = make(map[string]string)
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		password := scanner.Text()
+		sha256 := fmt.Sprintf("%x", sha256.Sum256([]byte(password)))
+		shalookup[password] = sha256
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -47,33 +87,42 @@ func GuessSingle(sourceHash string, filename string) {
 	}
 }
 
-func GenHashMaps(filename string) {
+func GenMD5HashMaps(filename string) {
+	md5lookup = make(map[string]string)
 
-	//TODO
-	//itterate through a file (look in the guessSingle function above)
-	//rather than check for equality add each hash:passwd entry to a map SHA and MD5 where the key = hash and the value = password
-	//TODO at the very least use go subroutines to generate the sha and md5 hashes at the same time
-	//OPTIONAL -- Can you use workers to make this even faster
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer f.Close()
 
-	//TODO create a test in hscan_test.go so that you can time the performance of your implementation
-	//Test and record the time it takes to scan to generate these Maps
-	// 1. With and without using go subroutines
-	// 2. Compute the time per password (hint the number of passwords for each file is listed on the site...)
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		password := scanner.Text()
+		md5 := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+		md5lookup[password] = md5
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
 }
 
 func GetSHA(hash string) (string, error) {
 	password, ok := shalookup[hash]
 	if ok {
 		return password, nil
-
 	} else {
-
 		return "", errors.New("password does not exist")
-
 	}
 }
 
-//TODO
 func GetMD5(hash string) (string, error) {
-	return "", errors.New("not implemented")
+	password, ok := md5lookup[hash]
+	if ok {
+		return password, nil
+	} else {
+		return "", errors.New("not implemented")
+	}
 }
--- materials/lab/7/hscan/hscan_test.go
+++ materials/lab/7/hscan/hscan_test.go
@@ -1,16 +1,26 @@
-// Optional Todo
-
 package hscan
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 func TestGuessSingle(t *testing.T) {
-	got := GuessSingle("77f62e3524cd583d698d51fa24fdff4f") // Currently function returns only number of open ports
-	want := "foo"
+	got := GuessSingle("90f2c9c53f66540e67349e0ab83d8cd0", "../main/Top304Thousand-probable-v2.txt")
+	want := "p@ssword"
 	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+		t.Errorf("got %s, wanted %s", got, want)
 	}
+}
 
+func TestGenHashMaps(t *testing.T) {
+	start := time.Now()
+	GenHashMaps("../main/Top304Thousand-probable-v2.txt")
+	duration := time.Since(start)
+
+	fmt.Println(duration.Nanoseconds())
+
+	// nonoseconds before any performance updates: 715893000 or 2355 per password
+	// nanoseconds with subroutines:			   619698700 or 2038 per password
 }
